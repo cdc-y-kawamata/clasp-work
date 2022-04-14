7228da11299addfffca4cab18a7aa71d26dbe6c8
y-kawamata <y-kawamata@type.jp>
2022-04-14 12:22:42 +0900

    testこけるtest

--- src/form/device-form.ts
+++ src/form/device-form.ts
@@ -6,7 +6,7 @@ export class DeviceForm extends BaseForm {
   private readonly startDate: string;
   private readonly endDate: string;
   constructor(formValues: { [key: string]: string[] }) {
-    super(formValues[FormItem.FORM_TIME_STAMP][0]);
+    super(formValues[FormItem.FORM_TIME_STAMP][1]);
     this.type = formValues[FormItem.DEVICE_FORM_TYPE][0];
     this.startDate = formValues[FormItem.DEVICE_FORM_START_DATE][0];
     this.endDate = formValues[FormItem.DEVICE_FORM_END_DATE][0];
